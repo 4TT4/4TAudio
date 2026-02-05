cf1d658ab21bae7844f31efe3d55beac8cd99c4a
Aarush Aggarwal <91485103+iam4tart@users.noreply.github.com>
2026-02-05 13:11:03 +0530

    Add rewrite for Google verification file

--- next.config.js
+++ next.config.js
@@ -2,4 +2,12 @@ module.exports = {
   images: {
     domains: ['www.notion.so', 'lh5.googleusercontent.com', 's3-us-west-2.amazonaws.com'],
   },
+  async rewrites() {
+    return [
+      {
+        source: '/google00e2a4034c56b95a.html',
+        destination: '/google00e2a4034c56b95a.html', // Ensure the file is served directly
+      },
+    ];
+  },
 }
