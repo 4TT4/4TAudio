22489f6a31b99fb98524ac12bbac3d12cc3f7d31
Aarush Aggarwal <91485103+iam4tart@users.noreply.github.com>
2026-02-05 13:15:05 +0530

    Add Google site verification meta tag to _app.tsx

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -2,12 +2,16 @@ import { AppPropsWithLayout } from "../types"
 import { Hydrate, QueryClientProvider } from "@tanstack/react-query"
 import { RootLayout } from "src/layouts"
 import { queryClient } from "src/libs/react-query"
+import Head from "next/head"
 
 function App({ Component, pageProps }: AppPropsWithLayout) {
   const getLayout = Component.getLayout || ((page) => page)
 
   return (
     <QueryClientProvider client={queryClient}>
+      <Head>
+        <meta name="google-site-verification" content="c1cLr6iKmJr1AO-vMbMkxBCxPyTVWGVnHLPcecfZ6b4" />
+      </Head>
       <Hydrate state={pageProps.dehydratedState}>
         <RootLayout>{getLayout(<Component {...pageProps} />)}</RootLayout>
       </Hydrate>
